a5fb3eb365a2440cc375e8864ef2eff850d676c6
MikkelMathiasen23 <51484172+MikkelMathiasen23@users.noreply.github.com>
2021-02-22 12:11:26 +0100

    Add files via upload

--- Assignment1/assignment_partA.R
+++ Assignment1/assignment_partA.R
@@ -1,14 +1,16 @@
 library(ggplot2)
 library(lmtest)
+library(car)
 
 df1=read.csv(file="./Data/clothingFull.csv")
 df2=read.csv(file="./Data/clothingSum.csv")
 
 df3 = df2[,c(3:6)]
 
+
 #Question 2:
 fit2 = lm( clo ~ tOut + tInOp + sex + tInOp*sex,data = df3)
-summary(fit2)
+Anova(fit2,type = 'III')
 
 df3$residual = resid(fit2)
 
@@ -22,6 +24,7 @@ ggplot(df3, aes(x = residual, colour = sex)) +
 'fit3 = lm( clo ~ tOut + tInOp + sex,data = df3)
 summary(fit3)
 '
+
 lrtest(fit2, fit3)
 
 df3$residual = NA
@@ -72,14 +75,18 @@ lines(tmp_male$tOut,conf[,2], col = 'red', lty = 2)
 lines(tmp_male$tOut,conf[,3], col = 'red', lty = 2)
 lines(tmp_male$tOut,pred[,2], col = 'green', lty = 2)
 lines(tmp_male$tOut,pred[,3], col = 'green', lty = 2)
+idx = df2$sex == 'male'
+points(df2$tOut[idx], df2$clo[idx], pch = 25)
 
 conf = predict(fit2,tmp_female,interval = 'confidence')
 pred = predict(fit2, tmp_female,interval = 'prediction')
-plot(tmp_male$tOut, conf[,1], col = 'blue', type = 'l',ylim = c(min(pred), max(pred)), ylab = 'clo', xlab = 'tOut')
-lines(tmp_female$tOut,conf[,2], col = 'red', lty = 2)
-lines(tmp_female$tOut,conf[,3], col = 'red', lty = 2)
-lines(tmp_female$tOut,pred[,2], col = 'green', lty = 2)
-lines(tmp_female$tOut,pred[,3], col = 'green', lty = 2)
+plot(tmp_male$tOut, conf[,1], col = 'blue',lwd = 2, type = 'l',ylim = c(min(pred), max(pred)), ylab = 'clo', xlab = 'tOut')
+lines(tmp_female$tOut,conf[,2], col = 'red',lwd = 2, lty = 2)
+lines(tmp_female$tOut,conf[,3], col = 'red',lwd = 2, lty = 2)
+lines(tmp_female$tOut,pred[,2], col = 'green',lwd = 2, lty = 2)
+lines(tmp_female$tOut,pred[,3], col = 'green',lwd = 2, lty = 2)
+idx2 = df2$sex == 'female'
+points(df2$tOut[idx2], df2$clo[idx2], pch = 25)
 
 
 
@@ -100,7 +107,7 @@ ggplot(data = df3, aes(x = idx, y = residual_2, col = as.factor(df2$subjId))) + 
 #Question 1 
 #Sex as this result in singular matrix 
 
-df2$subjId = as.factor(df2$subjId)
+df2$subjId = as.character(df2$subjId)
 fitB = lm( clo ~ tOut*tInOp+subjId,data = df2)
 summary(fitB)
 
@@ -117,15 +124,38 @@ qqline(resid(fit3B))
 
 
 #Question 2: 
-hist(fit3B$coefficients[3:length(fit3B$coefficients)])
-
+hist(fit3B$coefficients[3:length(fit3B$coefficients)], nclass= 12)
+l = length(fit3B$coefficients)
 
 qqnorm(fit3B$coefficients[3:length(fit3B$coefficients)])
 qqline(fit3B$coefficients[3:length(fit3B$coefficients)])
 
 
-#Question 3: 
+std_coef = sqrt(diag(vcov(fit3B)))[3:l]
+coef_est = fit3B$coefficients[3:l]
 
+df_coef = data.frame(cbind(coef_est, confint(fit3B)[3:l,]))
+df_coef$names =rownames(df_coef)
+df_coef$sex = NaN
+library(stringr)
+
+for (i in 1:(l-2)){
+  
+  df_coef$sex[i] = df2$sex[str_extract(df_coef$names[i],"([0-9]{1,3})")==df2$subjId] 
+}
+
+df_coef$names = as.numeric(str_extract(df_coef$names,"([0-9]{1,3})"))
+df_coef = df_coef[order(as.numeric(df_coef$names)),]
+df_coef$names = as.factor(df_coef$names)
+levels(df_coef$names)
+
+
+ggplot(data = df_coef, aes(x = names, y = coef_est, color = sex)) + 
+  geom_pointrange(aes(ymin=X2.5.., ymax=X97.5..)) + 
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+
+
+#Question 3: 
 
 
 ###############################################################################
